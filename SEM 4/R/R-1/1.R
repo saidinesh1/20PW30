db42584b3e8beb6aaff5ce15a7881a51efbc2b7d
saidinesh1 <76140435+saidinesh1@users.noreply.github.com>
2022-05-10 10:56:40 +0530

    May 10

--- SEM 4/R/R-1/1.R
+++ SEM 4/R/R-1/1.R
@@ -2,13 +2,13 @@
 library(lpSolve)
 
 # Set coefficients of the objective function
-f.obj <- c(5, 7)
+f.obj <- c(750,1000)
  
 # Set matrix corresponding to coefficients of constraints by rows
 # Do not consider the non-negative constraint; it is automatically assumed
-f.con <- matrix(c(1, 0,
-                  2, 3,
-                  1, 1), nrow = 3, byrow = TRUE)
+f.con <- matrix(c(1, 1,
+                  1, 2,
+                  4, 3), nrow = 3, byrow = TRUE)
 
 # Set unequality signs
 f.dir <- c("<=",
@@ -16,9 +16,9 @@ f.dir <- c("<=",
            "<=")
 
 # Set right hand side coefficients
-f.rhs <- c(16,
-           19,
-           8)
+f.rhs <- c(10000,
+           15000,
+           25000)
 
 # Final value (z)
 lp("max", f.obj, f.con, f.dir, f.rhs)
